56b1b3cdb3d7d693a830f8dcc095999873f7871e
gujarati2003 <126843012+gujarati2003@users.noreply.github.com>
2023-03-02 22:23:01 -0500

    added a comment

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -6,6 +6,7 @@ public class Coin : MonoBehaviour
 {
 public int coinValue = 1;
 
+// i am going to save it
 // When the player hits the object set to other the scoreManager script will activate and will also disapear the coin
     private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Player")){
